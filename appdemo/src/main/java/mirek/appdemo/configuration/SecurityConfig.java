fad736a78a1599faae3e025c50000fa13aeb7f62
MirekTuruk <miroslaw.turuk@gmail.com>
2019-03-19 22:15:31 +0100

    m

--- appdemo/src/main/java/mirek/appdemo/configuration/SecurityConfig.java
+++ appdemo/src/main/java/mirek/appdemo/configuration/SecurityConfig.java
@@ -1,6 +1,6 @@
 package mirek.appdemo.configuration;
 
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,54 +15,53 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-	@EnableWebSecurity
-	// @EnableGlobalMethodSecurity(securedEnabled = true)
-	public class SecurityConfig extends WebSecurityConfigurerAdapter {
-		
-		@Autowired
-		private BCryptPasswordEncoder bcp;
-		
-		@Autowired
-		private DataSource ds;
-		
-		@Value("${spring.queries.users-query}")
-		private String usersQuery;
-		
-		@Value("${spring.queries.roles-query}")
-		private String rolesQuery;
-		
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.jdbcAuthentication().usersByUsernameQuery(usersQuery)
-			.authoritiesByUsernameQuery(rolesQuery)
-			.dataSource(ds).passwordEncoder(bcp);
-		}
-		
-		protected void configure(HttpSecurity httpSec) throws Exception {
-			httpSec
-			.authorizeRequests()
-			.antMatchers("/").permitAll()
-			.antMatchers("/index").permitAll()
-			.antMatchers("/login").permitAll()
-			.antMatchers("/register").permitAll()
-			.antMatchers("/adduser").permitAll()
-			.antMatchers("/activatelink/**").permitAll()
-//			.antMatchers("/admin").hasAuthority("ROLE_ADMIN")
-			.anyRequest().authenticated()
-			.and().csrf().disable()
-			.formLogin()
-			.loginPage("/login")
-			.failureUrl("/login?error=true")
-			.defaultSuccessUrl("/").usernameParameter("email")
-			.passwordParameter("password")
-			.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutSuccessUrl("/")
-			.and().exceptionHandling().accessDeniedPage("/denied");
-		}
-		
-		public void configure(WebSecurity webSec) throws Exception {
-			webSec.ignoring()
-			.antMatchers("/resources/**", "/statics/**", "/css/**", "/js/**", "/images/**", "/incl/**");
-		}
-
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private BCryptPasswordEncoder bcp;
+	
+	@Autowired
+	private DataSource ds;
+	
+	@Value("${spring.queries.users-query}")
+	private String usersQuery;
+	
+	@Value("${spring.queries.roles-query}")
+	private String rolesQuery;
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery)
+		.authoritiesByUsernameQuery(rolesQuery)
+		.dataSource(ds).passwordEncoder(bcp);
+	}
+	
+	protected void configure(HttpSecurity httpSec) throws Exception {
+		httpSec
+		.authorizeRequests()
+		.antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/register").permitAll()
+		.antMatchers("/adduser").permitAll()
+		.antMatchers("/activatelink/**").permitAll()
+//		.antMatchers("/admin").hasAuthority("ROLE_ADMIN")
+		.anyRequest().authenticated()
+		.and().csrf().disable()
+		.formLogin()
+		.loginPage("/login")
+		.failureUrl("/login?error=true")
+		.defaultSuccessUrl("/").usernameParameter("email")
+		.passwordParameter("password")
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/")
+		.and().exceptionHandling().accessDeniedPage("/denied");
+	}
+	
+	public void configure(WebSecurity webSec) throws Exception {
+		webSec.ignoring()
+		.antMatchers("/resources/**", "/statics/**", "/css/**", "/js/**", "/images/**", "/incl/**");
 	}
 
+}
